5883c6fdcf66487ac7799b70747496b7fe1e3b1f
Alexandre Vasseur <avasseur@vmware.com>
2014-12-17 11:48:30 +0100

    orange

--- src/main/java/org/example/cities/VersionController.java
+++ src/main/java/org/example/cities/VersionController.java
@@ -8,6 +8,6 @@ public class VersionController {
 
     @RequestMapping("/version")
     public String index() {
-        return "Greetings Paris Roadshow from Alex!";
+        return "Greetings Paris Roadshow from Alex for Orange!";
     }
 }
