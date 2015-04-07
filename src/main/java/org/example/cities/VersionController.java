831afa832c2797d10691b32990cb5cc9f4ef4504
user <user@centos62.semea.piv>
2015-04-07 10:48:44 +0200

    commit v11

--- src/main/java/org/example/cities/VersionController.java
+++ src/main/java/org/example/cities/VersionController.java
@@ -8,6 +8,6 @@ public class VersionController {
 
     @RequestMapping("/version")
     public String index() {
-        return "Continuous delivery v2";
+        return "Continuous delivery v11";
     }
 }
