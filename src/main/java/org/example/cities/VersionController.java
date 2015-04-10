370055fbb0a0079a33fc282eefb0289e6a2ca8ab
user <user@centos62.semea.piv>
2015-04-10 16:20:14 +0200

    toto

--- src/main/java/org/example/cities/VersionController.java
+++ src/main/java/org/example/cities/VersionController.java
@@ -8,6 +8,6 @@ public class VersionController {
 
     @RequestMapping("/version")
     public String index() {
-        return "Continuous delivery v11";
+        return "Continuous delivery v12";
     }
 }
