07181544bb5e2a5e224bd9bec8fca300617c5f18
user <user@centos62.semea.piv>
2015-05-13 13:12:37 +0200

    v14

--- src/main/java/org/example/cities/VersionController.java
+++ src/main/java/org/example/cities/VersionController.java
@@ -8,6 +8,6 @@ public class VersionController {
 
     @RequestMapping("/version")
     public String index() {
-        return "Continuous delivery v13";
+        return "Continuous delivery v14";
     }
 }
